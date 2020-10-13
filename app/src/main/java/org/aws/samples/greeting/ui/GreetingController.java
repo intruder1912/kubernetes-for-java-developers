860b7484ed3dfd61e37b727162a3eade4f36ec1e
Intruder <6000952+intruder1912@users.noreply.github.com>
2020-10-13 21:52:46 +0200

    added optional request param to controller and updated docker base images to newer ones employing java v14

--- app/src/main/java/org/aws/samples/greeting/ui/GreetingController.java
+++ app/src/main/java/org/aws/samples/greeting/ui/GreetingController.java
@@ -1,7 +1,10 @@
 package org.aws.samples.greeting.ui;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletRequest;
 
 /**
  * @author Arun Gupta
@@ -10,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GreetingController {
 
     @RequestMapping("/hello")
-    public String sayHello() {
-        return "Hello!";
+    public String sayHello(@RequestParam(required = false) String name) {
+        if (name == null) {
+            return "Hello Somebody!";
+        } else {
+            return new StringBuilder().append("Hello ").append(name).append("!").toString();
+        }
     }
 }
